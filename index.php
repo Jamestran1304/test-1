26a610e1887a7b097e35edaa6b520a5499f10ab1
Jamestran1304 <62641837+Jamestran1304@users.noreply.github.com>
2020-04-22 16:49:26 +0700

    index

---
+++ index.php
@@ -0,0 +1,11 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8" />
+    <title>PHP small app</title>
+</head>
+<body>
+    <?php echo'<p>Hello. This is a demo web page</p>';?>
+</body>
+</html>
